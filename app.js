6c8da8432bf713908eb752edf4d9158f54b3c7ee
jvelazquez <jonathanvgms@gmail.com>
2022-07-13 11:00:47 -0300

    Agregado endpoints

--- app.js
+++ app.js
@@ -10,13 +10,33 @@ app.use(express.json())
 //define un puerto en que va a escuchar pedidos
 const port = 3000
 
-app.get('/', (pedido, respuesta) => {
-    respuesta.send('mensaje bienvenida')
+let usuarios = [
+    { email: "pablo@mail.com", "usuario": "pablo", contrasenia: "1234" },
+    { email: "juan@mail.com", "usuario": "juan", contrasenia: "2345" },
+    { email: "jose@mail.com", "usuario": "jose", contrasenia: "3456" }]
+
+//endpoints
+app.get('/usuarios', (pedido, respuesta) => {
+    respuesta.send(usuarios)
 })
 
-app.post('/', (pedido, respuesta) => {
-    console.log(pedido.body)
+app.get('/usuarios/:id', (pedido, respuesta) => {
+    let id = pedido.params.id
+    //console.log(id)
+    respuesta.send(usuarios.filter(x => x.email == id))
+})
+
+app.post('/usuarios', (pedido, respuesta) => {
+    usuarios.push(pedido.body)
     respuesta.send(pedido.body)
+})
+
+app.put('/usuarios/:id', (pedido, respuesta) => {
+    let id = pedido.params.id
+    let usuario = usuarios.filter(x => x.email == id).at(0)
+    usuario.usuario = pedido.body.usuario
+    usuario.contrasenia = pedido.body.contrasenia
+    respuesta.send(usuario)
 })
 
 app.listen(port)
