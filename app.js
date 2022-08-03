8db63320c29bb474cd9cda76764905ff6abd65e8
jvelazquez <jonathanvgms@gmail.com>
2022-08-03 10:13:51 -0300

    Add roles to user

--- app.js
+++ app.js
@@ -11,9 +11,25 @@ app.use(express.json())
 const port = 3000
 
 let usuarios = [
-    { email: "pablo@mail.com", "usuario": "pablo", contrasenia: "1234" },
-    { email: "juan@mail.com", "usuario": "juan", contrasenia: "2345" },
-    { email: "jose@mail.com", "usuario": "jose", contrasenia: "3456" }]
+    {
+        email: "pablo@mail.com",
+        usuario: "pablo",
+        contrasenia: "1234",
+        roles: ["administrador", "vendedor"]
+    },
+    {
+        email: "juan@mail.com",
+        usuario: "juan",
+        contrasenia: "2345",
+        roles: ["administrador"]
+    },
+    {
+        email: "jose@mail.com",
+        usuario: "jose",
+        contrasenia: "3456",
+        roles: ["vendedor"]
+    }
+]
 
 //endpoints
 app.get('/usuarios', (pedido, respuesta) => {
@@ -50,5 +66,15 @@ app.delete('/usuarios/:id', (request, response) => {
     usuarios.splice(indice, 1)
     response.send("Se elimino el usuario")
 });
+
+app.get('/usuarios/:email/roles', (request, response) => {
+    let email = request.params.email
+    let usuario = usuarios.filter(x => x.email == email).at(0)
+
+    if (usuario == null)
+        response.status(404).send("No se encuentra el usuario")
+
+    response.send(usuario.roles)
+})
 
 app.listen(port)
